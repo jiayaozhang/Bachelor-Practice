f3f0bcdd80b3023fb943955f4d1656c20269f813
jiayaozhang <38579506+jiayaozhang@users.noreply.github.com>
2019-03-20 09:42:13 +0800

    Update demo_1.m

--- EMBC_Novel detection and image segmentation/DRLSE_v0/demo_1.m
+++ EMBC_Novel detection and image segmentation/DRLSE_v0/demo_1.m
@@ -1,13 +1,4 @@
-%  This Matlab code demonstrates an edge-based active contour model as an application of 
-%  the Distance Regularized Level Set Evolution (DRLSE) formulation in the following paper:
-%
-%  C. Li, C. Xu, C. Gui, M. D. Fox, "Distance Regularized Level Set Evolution and Its Application to Image Segmentation", 
-%     IEEE Trans. Image Processing, vol. 19 (12), pp. 3243-3254, 2010.
-%
-% Author: Chunming Li, all rights reserved
-% E-mail: lchunming@gmail.com   
-%         li_chunming@hotmail.com 
-% URL:  http://www.imagecomputing.org/~cmli//
+
 
 clear all;
 close all;
